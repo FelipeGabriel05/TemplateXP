bef05b532a710e39811d1911c1572d90d5a00172
Felipe Gabriel <133790793+FelipeGabriel05@users.noreply.github.com>
2023-12-13 11:23:05 -0300

    Update connet.py

--- connet.py
+++ connet.py
@@ -1,7 +1,7 @@
 import main
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://felipegabrielgb271:felipe123@cluster0.rsejnr3.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://seu_email:senha.rsejnr3.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
